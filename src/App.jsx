da3d61d0a13aabc82c93735624ae2fa5350853a9
escapingmatr <js.scott.park@gmail.com>
2024-11-06 23:14:28 -0500

    Product Details page setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Landing from '@/pages/index.jsx';
 import Products from '@/pages/products/index.jsx';
+import ProductDetails from '@/pages/products/product_details/index.jsx';
 
 function App() {
   return (
     <>
       <div className="app">
-        <Products />
+        <ProductDetails />
       </div>
     </>
   );
---
+++ src/pages/products/product_details/index.jsx
@@ -0,0 +1,12 @@
+import Flickity from 'flickity';
+import '@/pages/products/product_details/index.scss';
+
+function ProductDetails() {
+  return (
+    <div className="product-details page">
+      <div>product details placeholder</div>
+    </div>
+  );
+}
+
+export default ProductDetails;

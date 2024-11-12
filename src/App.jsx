5ce2a26a377ac7d27549a234cbb64aba15c000a7
escapingmatr <js.scott.park@gmail.com>
2024-11-12 02:52:16 -0500

    ProductInfo migrated under ProductGallery, and ProductDetails is cleaned up

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import Landing from '@/pages/index.jsx';
 import Products from '@/pages/products/index.jsx';
 import ProductDetails from '@/pages/products/product_details/index.jsx';
-import ProductGallery from './pages/products/product_details/product_gallery';
+// import ProductGallery from './pages/products/product_details/product_gallery';
 
 function App() {
   return (
     <>
       <div className="app">
-        <ProductGallery />
+        <ProductDetails />
       </div>
     </>
   );

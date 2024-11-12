e368ddc527ea8556f4df2abfad11741960e826d3
escapingmatr <js.scott.park@gmail.com>
2024-11-12 03:05:48 -0500

    added notes on how ProductGallery becomes responsive

--- src/pages/products/product_details/product_gallery/index.jsx
+++ src/pages/products/product_details/product_gallery/index.jsx
@@ -7,6 +7,9 @@ import ProductInfo from '@/pages/products/product_details/product_gallery/produc
 import '@/pages/products/product_details/product_gallery/index.scss';
 
 // TODO: Make ProductGallery Responsive
+// at 1139px, carousel-nav should disappear
+// at 1007px, carousel-main becomes normal horizontal carousel
+// at 1007px, the flex-direction changes to column
 
 function ProductGallery({ product }) {
   const images = product.images;

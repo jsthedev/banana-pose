c55dbbbc09d6b4983b8492ed74a44ec456bdcf06
escapingmatr <js.scott.park@gmail.com>
2024-11-12 22:09:42 -0500

    mid and wide screen gallery implemented, to be tested

---
+++ src/pages/products/product_details/product_gallery/product_gallery_screen_sizes/mid_screen_gallery/index.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useRef, useState } from 'react';
+import Flickity from 'flickity';
+import 'flickity/css/flickity.css';
+
+import ProductInfo from '@/pages/products/product_details/product_gallery/product_info/index.jsx';
+
+import '@/pages/products/product_details/product_gallery/product_gallery_screen_sizes/mid_screen_gallery/index.scss';
+
+function MidScreenGallery({ product }) {
+  const images = product.images;
+
+  // Flickity Refs
+  const mainCarouselRef = useRef(null);
+
+  useEffect(() => {
+    // Flickity Initialization
+    const mainFlkty = new Flickity(mainCarouselRef.current, {
+      prevNextButtons: false,
+      pageDots: false,
+      adaptiveHeight: true,
+      watchCSS: true,
+      dragThreshold: 8,
+      initialIndex: 0,
+      draggable: true,
+    });
+
+    // Unmount Cleanup
+    return () => {
+      mainFlkty.destroy();
+    };
+  }, []);
+
+  return (
+    <div className="mid-screen-gallery">
+      <div className="carousel-main" ref={mainCarouselRef}>
+        {images.map((photo, index) => (
+          <div
+            className="carousel-cell"
+            key={index}
+            id={photo}
+            ref={(currentImage) =>
+              (currentImageRefs.current[index] = currentImage)
+            }
+          >
+            <img src={photo} alt={`Thumbnail ${index + 1}`} />
+          </div>
+        ))}
+      </div>
+      <div className="product-info-container">
+        <ProductInfo product={product} />
+      </div>
+    </div>
+  );
+}
+
+export default MidScreenGallery;
---
+++ src/pages/products/product_details/product_gallery/product_gallery_screen_sizes/small_screen_gallery/index.jsx
@@ -0,0 +1,11 @@
+import '@/pages/products/product_details/product_gallery/product-gallery_screen_sizes/small_screen_gallery/index.scss';
+
+function SmallScreenGallery() {
+  return (
+    <div className="small-screen-gallery">
+      <div>small-screen-gallery placeholder</div>
+    </div>
+  );
+}
+
+export default SmallScreenGallery;

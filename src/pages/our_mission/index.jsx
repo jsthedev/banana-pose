a6567db1b1889aa391919186434e9fc18a432460
escapingmatr <js.scott.park@gmail.com>
2024-11-22 01:18:35 -0500

    added placeholder image and set up title

--- src/pages/our_mission/index.jsx
+++ src/pages/our_mission/index.jsx
@@ -1,9 +1,16 @@
 import '@/pages/our_mission/index.scss';
 
+import Placeholder from '@/assets/images/our_mission/35242676104_61abae6693_o.jpg';
+
 function OurMission() {
   return (
     <div className="our-mission page">
-      <div className="our-mission-title">Our Mission</div>
+      <div className="full-screen-title">
+        <div className="wide-image-wrapper">
+          <img src={Placeholder} />
+        </div>
+        <div className="our-mission-title">Our Mission</div>
+      </div>
     </div>
   );
 }

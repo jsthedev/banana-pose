b0ad5f67230274766a85e72dab18b99d7a7fa734
escapingmatr <js.scott.park@gmail.com>
2024-11-23 00:23:29 -0500

    main text added to our mission

--- src/pages/our_mission/index.jsx
+++ src/pages/our_mission/index.jsx
@@ -1,6 +1,7 @@
 import '@/pages/our_mission/index.scss';
 
 import Placeholder from '@/assets/images/our_mission/35242676104_61abae6693_o.jpg';
+import LogoImage from '@/assets/images/logo/seal_logo.png';
 
 function OurMission() {
   return (
@@ -10,7 +11,24 @@ function OurMission() {
           <img src={Placeholder} />
         </div>
         <div className="our-mission-title">Our Mission</div>
+        <div className="full-screen-title-spacer" />
       </div>
+      <div className="our-mission-main-text">
+        <div className="logo-image-wrapper">
+          <img src={LogoImage} />
+        </div>
+        <h3>
+          Seals only perform the banana pose when they feel safe and content.
+        </h3>
+        <p>
+          We draw inspiration from this serene behavior to create clothing that
+          brings you the same sense of comfort and contentedness. <br />
+          <br />
+          Our mission is to craft apparel that provides comforting and contented
+          experiences.
+        </p>
+      </div>
+      <div className="photo-text-boxes"></div>
     </div>
   );
 }

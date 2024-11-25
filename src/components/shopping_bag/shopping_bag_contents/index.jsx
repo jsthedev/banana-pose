ed1213d9e538ee2b51879b09f3b28f63e8d704e7
escapingmatr <js.scott.park@gmail.com>
2024-11-24 20:40:04 -0500

    responsive design applied

--- src/components/shopping_bag/shopping_bag_contents/index.jsx
+++ src/components/shopping_bag/shopping_bag_contents/index.jsx
@@ -22,43 +22,48 @@ function ShoppingBagContents() {
 
   return (
     <div className="shopping-bag-contents">
-      <div className="column-spacer"></div>
-      <div className="shopping-bag-item-list">
-        {state.shoppingBagItems.map((item) => (
-          <div className="shopping-bag-item" key={`${item.id}-${item.size}`}>
-            <div className="item-thumbnail">
-              <img src={item.thumbnail} alt={`${item.thumbnail}`} />
-            </div>
-            <div className="shopping-bag-item-contents-wrapper">
-              <div className="top-content">
-                <div className="item-details">
-                  <div className="item-name item-detail">{item.name}</div>
-                  <div className="item-size item-detail">Size: {item.size}</div>
-                  <div className="item-quantity item-detail">
-                    Quantity: {item.quantity}
+      <div className="column-spacer left-column"></div>
+      <div className="shopping-bag-item-list-wrapper">
+        <div className="shopping-bag-page-name">Shopping bag</div>
+        <div className="shopping-bag-item-list">
+          {state.shoppingBagItems.map((item) => (
+            <div className="shopping-bag-item" key={`${item.id}-${item.size}`}>
+              <div className="item-thumbnail">
+                <img src={item.thumbnail} alt={`${item.thumbnail}`} />
+              </div>
+              <div className="shopping-bag-item-contents-wrapper">
+                <div className="top-content">
+                  <div className="item-details">
+                    <div className="item-name item-detail">{item.name}</div>
+                    <div className="item-size item-detail">
+                      Size: {item.size}
+                    </div>
+                    <div className="item-quantity item-detail">
+                      Quantity: {item.quantity}
+                    </div>
+                  </div>
+                  <div className="item-price">${item.price}</div>
+                </div>
+                <div className="bottom-content">
+                  <div
+                    className="remove-button normal-link"
+                    onClick={() => removeItem(item.id, item.size)}
+                  >
+                    Remove
                   </div>
                 </div>
-                <div className="item-price">${item.price}</div>
-              </div>
-              <div className="bottom-content">
-                <div
-                  className="remove-button normal-link"
-                  onClick={() => removeItem(item.id, item.size)}
-                >
-                  Remove
-                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-      <div className="column-spacer"></div>
+      <div className="column-spacer right-column"></div>
       <div className="shopping-bag-sidebar">
-        <div className="order-total sidebar-line">
+        <div className="order-total">
           <div className="order-total-text">Total:</div>
           <div className="order-total-dollar">$ {total}</div>
         </div>
-        <div className="shipping-cost-warning sidebar-line">
+        <div className="shipping-cost-warning">
           Shipping cost calculated at the checkout
         </div>
         <div className="checkout-button link-button">PROCEED TO CHECKOUT</div>

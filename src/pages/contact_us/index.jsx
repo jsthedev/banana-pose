36a6ce5ad8ecce39b4b3aa54ddd16e08934a5326
escapingmatr <js.scott.park@gmail.com>
2024-11-26 21:17:02 -0500

    responsive contact us page

---
+++ src/pages/contact_us/index.jsx
@@ -0,0 +1,26 @@
+import '@/pages/contact_us/index.scss';
+
+function ContactUs() {
+  return (
+    <div className="contact-us page">
+      <div className="page-name">Contact Us</div>
+      <div className="contact-list">
+        <div className="email contact-list-item">
+          <h3>Email</h3>
+          <p>info@bananapose.com</p>
+        </div>
+        <div className="address contact-list-item">
+          <h3>Address</h3>
+          <p>
+            Suite 314 <br />
+            818 The Queensway <br />
+            Toronto ON M8Z 1N5 <br />
+            Canada
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ContactUs;

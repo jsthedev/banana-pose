44cd6b06b01c10cb353fc62b1c5fabe472a1b3d3
escapingmatr <js.scott.park@gmail.com>
2025-01-16 21:36:05 -0500

    size chart drawer fix

--- src/components/product_details/size_chart_drawer/index.jsx
+++ src/components/product_details/size_chart_drawer/index.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState, useImperativeHandle, forwardRef } from 'react';
+import { useContext } from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';

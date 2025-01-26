9a07171a982c20d53eb8ef8d0a421bedb8e3c4fa
escapingmatr <js.scott.park@gmail.com>
2025-01-26 17:24:54 -0500

    Stripe Webhook functions setup

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,9 @@
 const cors = require('cors');
 const { onRequest } = require('firebase-functions/v2/https');
 const { defineSecret } = require('firebase-functions/params');
+const admin = require('firebase-admin');
+const { initializeApp } = require('firebase-admin/app');
+const Stripe = require('stripe');
 
 const stripeSecret = defineSecret('STRIPE_SECRET');
 const ipinfoToken = defineSecret('IPINFO_TOKEN');
@@ -18,6 +21,7 @@ const {
   write,
 } = require('firebase-functions/logger');
 
+admin.initializeApp();
 const YOUR_DOMAIN = 'https://bananapose.com'; // Adjust as needed
 
 exports.getCurrency = onRequest({ secrets: [ipinfoToken] }, (req, res) => {
@@ -73,7 +77,7 @@ exports.createCheckoutSession = onRequest(
           return res.status(400).send({ error: 'Unsupported currency.' });
         }
 
-        const stripe = require('stripe')(stripeSecret.value());
+        const stripe = Stripe(stripeSecret.value());
 
         // Create a promise to find price IDs from Stripe
         const lineItemPromises = shoppingBagItems.map(async (item) => {
@@ -141,7 +145,7 @@ exports.sessionStatus = onRequest({ secrets: [stripeSecret] }, (req, res) => {
 
   corsHandler(req, res, async () => {
     try {
-      const stripe = require('stripe')(stripeSecret.value());
+      const stripe = Stripe(stripeSecret.value());
       const session = await stripe.checkout.sessions.retrieve(
         req.query.session_id
       );
@@ -154,3 +158,112 @@ exports.sessionStatus = onRequest({ secrets: [stripeSecret] }, (req, res) => {
     }
   });
 });
+
+exports.handleStripeWebhook = onRequest(
+  { secrets: [stripeSecret] },
+  (req, res) => {
+    const corsHandler = cors({ origin: true });
+
+    corsHandler(req, res, async () => {
+      const stripe = Stripe(stripeSecret.value());
+
+      const stripeSignature = req.headers['stripe-signature'];
+
+      let event;
+
+      try {
+        // Verify the event came from Stripe
+        event = stripe.webhooks.constructEvent(
+          req.rawBody,
+          stripeSignature,
+          endpointSecret
+        );
+      } catch (error) {
+        console.error(
+          '⚠️  Webhook signature verification failed.',
+          error.message
+        );
+        return res.status(400).send(`Webhook Error: ${error.message}`);
+      }
+
+      // Handle the checkout.session.completed event
+      if (event.type === 'checkout.session.completed') {
+        const session = event.data.object;
+
+        // Fulfill the purchase...
+        try {
+          await fulfillOrder(session);
+          res.status(200).send('Success');
+        } catch (err) {
+          console.error('Error fulfilling order:', err);
+          res.status(500).send(`Webhook Error: ${err.message}`);
+        }
+      } else {
+        // Unexpected event type
+        res.status(400).end();
+      }
+    });
+  }
+);
+
+async function fulfillOrder(session) {
+  const stripe = Stripe(stripeSecret.value());
+
+  // Retrieve line items for the session
+  const lineItems = await stripe.checkout.sessions.listLineItems(session.id, {
+    limit: 100,
+  });
+
+  const db = admin.firestore();
+
+  // Create promises to update inventory based on each line item
+  const updatePromises = lineItems.data.map(async (item) => {
+    const stripeProductId = item.price.product;
+    const quantity = item.quantity;
+
+    const searchedProduct = await stripe.products.search({
+      query: `active:'true' AND id:'${stripeProductId}'`,
+    });
+
+    const productId = searchedProduct.metadata.bp_product_id;
+    const variantId = searchedProduct.metadata.variant_id;
+    const sizeId = searchedProduct.metadata.size;
+
+    if (!productId || !variantId || !sizeId) {
+      throw new Error(
+        'Missing product, variant, or size information in line item.'
+      );
+    }
+
+    const sizeRef = db
+      .collection('products')
+      .doc(productId)
+      .collection('variants')
+      .doc(variantId)
+      .collection('sizes')
+      .doc(sizeId);
+
+    // Create a transaction to decrement inventory on Firestore DB
+    await db.runTransaction(async (transaction) => {
+      const sizeDoc = await transaction.get(sizeRef);
+      if (!sizeDoc.exists) {
+        throw new Error(
+          `Size ${sizeId} does not exist for variant ${variantId}.`
+        );
+      }
+
+      const currentInventory = sizeDoc.data().inventory || 0;
+      const newInventory = currentInventory - quantity;
+
+      if (newInventory < 0) {
+        throw new Error(
+          `Insufficient inventory for product ${productId}, variant ${variantId}, size ${sizeId}.`
+        );
+      }
+
+      // Update the inventory
+      transaction.update(sizeRef, { inventory: newInventory });
+    });
+  });
+  await Promise.all(updatePromises);
+}

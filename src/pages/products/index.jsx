52a72048812e3c7d19573a60b89d54a484c0d039
escapingmatr <js.scott.park@gmail.com>
2025-02-24 00:11:22 -0500

    products page quick fix

--- src/pages/products/index.jsx
+++ src/pages/products/index.jsx
@@ -1,11 +1,11 @@
-import { useContext } from 'react';
+import { useContext } from "react";
 
-import ProductCard from '@/components/products/product_card/index.jsx';
-import LoadingProductCard from '@/components/products/product_card/loading_product_card';
+import ProductCard from "@/components/products/product_card/index.jsx";
+import LoadingProductCard from "@/components/products/product_card/loading_product_card";
 
-import { ProductsContext } from '@/contexts/productsContext';
+import { ProductsContext } from "@/contexts/productsContext";
 
-import '@/pages/products/index.scss';
+import "@/pages/products/index.scss";
 
 function Products() {
   // Contexts
@@ -16,13 +16,13 @@ function Products() {
       <div className="products-page-main">
         <div className="product-list-wrapper">
           <div className="product-list">
-            {Object.keys(products).map((productId) =>
-              productsLoading ? (
-                <LoadingProductCard key={`${productId}`} />
-              ) : (
-                <ProductCard key={`${productId}`} productId={productId} />
-              )
-            )}
+            {productsLoading
+              ? Array.from({ length: import.meta.env.VITE_NUM_PRODUCTS }).map(
+                  (_, index) => <LoadingProductCard key={index} />
+                )
+              : Object.keys(products).map((productId) => (
+                  <ProductCard key={`${productId}`} productId={productId} />
+                ))}
           </div>
         </div>
       </div>
